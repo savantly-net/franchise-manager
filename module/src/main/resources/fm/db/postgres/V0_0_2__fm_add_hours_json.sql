c1c5d265698ceb7c5421ab2e7a03d0c8ff29c4db
Jeremy Branham <Jeremy@Savantly.net>
2021-09-23 23:27:23 -0500

    use correct exception code

--- module/src/main/resources/fm/db/postgres/V0_0_2__fm_add_hours_json.sql
+++ module/src/main/resources/fm/db/postgres/V0_0_2__fm_add_hours_json.sql
@@ -1,11 +1,8 @@
 DO $$
 BEGIN
-
-  BEGIN
-    ALTER TABLE fm_location ADD COLUMN hours jsonb;
-  EXCEPTION
-    WHEN duplicate_object THEN RAISE NOTICE 'fm_location.hours column already exists';
-  END;
+	ALTER TABLE fm_location ADD COLUMN hours jsonb;
+EXCEPTION
+    WHEN duplicate_column THEN RAISE NOTICE 'fm_location.hours column already exists';
 END $$;
 
 DROP TABLE IF EXISTS fm_operating_hours CASCADE;
