f0c85ae91ed2191f7fbec7c546fc9ffe9cd1aeae
Jeremy Branham <Jeremy@Savantly.net>
2021-09-23 22:31:32 -0500

    fix sql syntax and make migration repeatable

--- module/src/main/resources/fm/db/postgres/V0_0_2__fm_add_hours_json.sql
+++ module/src/main/resources/fm/db/postgres/V0_0_2__fm_add_hours_json.sql
@@ -1,2 +1,11 @@
-ALTER TABLE fm_location ADD COLUMN hours jsonb
-DROP TABLE IF EXISTS fm_operating_hours
+DO $$
+BEGIN
+
+  BEGIN
+    ALTER TABLE fm_location ADD COLUMN hours jsonb;
+  EXCEPTION
+    WHEN duplicate_object THEN RAISE NOTICE 'fm_location.hours column already exists';
+  END;
+END $$;
+
+DROP TABLE IF EXISTS fm_operating_hours CASCADE;
