82fb85b977185bae3ed9972243b8b8983cfef262
Manoj <p.manoj.vivek+savantly@gmail.com>
2021-09-28 20:28:17 +0530

    QAI Sections Editor UI tweaks

--- module/src/plugin/pages/Quality/sections/item/editor.tsx
+++ module/src/plugin/pages/Quality/sections/item/editor.tsx
@@ -1,6 +1,10 @@
+import { IconButton, Text, Box } from '@chakra-ui/react';
+import { Table, TableCaption, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/table';
+import { AddIcon, DeleteIcon } from '@chakra-ui/icons';
 import { confirm, FormField, Icon, ItemEditorProps } from '@sprout-platform/ui';
 import { css, cx } from 'emotion';
 import { Field, FieldArray, Form, Formik, FormikProps } from 'formik';
+import FormikTextInput from 'plugin/components/FormikTextInput';
 import { AppModuleRootState } from 'plugin/types';
 import React, { Fragment, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -27,40 +31,66 @@ const QuestionEditor = (props: FormikProps<QAISection>) => {
   return (
     <FieldArray name="questions">
       {({ insert, remove, push }) => (
-        <Fragment>
-          <div
-            className={cx(
-              'mb-2',
-              css`
-                display: flex;
-              `
-            )}
-          >
-            <h4>Inspection Questions</h4>
-            <Button
-              className="ml-1"
-              color="info"
-              onClick={() => {
-                push({
-                  text: '',
-                  points: 2,
-                });
-              }}
-            >
-              <Icon name="plus" size="1x" />
-            </Button>
+        <Box p={4}>
+          <div>
+            <Text fontSize="l">Inspection Questions</Text>
           </div>
           <div>
-            {props.values.questions &&
-              props.values.questions.length > 0 &&
-              props.values.questions.map((question, index) => (
-                <Card className="card mb-1" key={`questions-${index}`}>
-                  <CardBody>
-                    <Row>
-                      <div className=" form-group col-1 d-flex align-items-end">
-                        <Button
-                          outline={false}
-                          color="danger"
+            <Table>
+              <TableCaption>
+                Add a new inspection question{' '}
+                <IconButton
+                  aria-label="Add Inspection Question"
+                  icon={<AddIcon />}
+                  onClick={() => {
+                    push({
+                      text: '',
+                      points: 2,
+                    });
+                  }}
+                />
+              </TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Question Text</Th>
+                  <Th>Category</Th>
+                  <Th>Points</Th>
+                  <Th>Actions</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {props.values.questions &&
+                  props.values.questions.length > 0 &&
+                  props.values.questions.map((question, index) => (
+                    <Tr key={`questions.${index}.text`}>
+                      <Td>
+                        <Field
+                          name={`questions.${index}.text`}
+                          component={FormikTextInput}
+                          placeholder="Enter Question"
+                        />
+                      </Td>
+                      <Td>
+                        <FormField className="form-group col-9" as="select" name={`questions.${index}.categoryId`}>
+                          <Fragment>
+                            <option></option>
+                            {categoryState.response?.content &&
+                              categoryState.response.content.map((t, index) => (
+                                <option key={index} value={t.id?.itemId}>
+                                  {t.name}
+                                </option>
+                              ))}
+                          </Fragment>
+                        </FormField>
+                      </Td>
+                      <Td>
+                        <Field name={`questions.${index}.points`} component={FormikTextInput} />
+                      </Td>
+                      <Td isNumeric>
+                        <IconButton
+                          colorScheme="red"
+                          aria-label="Delete Question"
+                          icon={<DeleteIcon />}
                           onClick={() => {
                             confirm({
                               message: 'Delete this question?',
@@ -70,46 +100,14 @@ const QuestionEditor = (props: FormikProps<QAISection>) => {
                               }
                             });
                           }}
-                        >
-                          <Icon name="trash" size="1x" />
-                        </Button>
-                      </div>
-                      <div className="form-group col-11">
-                        <label htmlFor={`questions.${index}.text`} className="mr-2">
-                          Text
-                        </label>
-                        <Field name={`questions.${index}.text`} className="form-control" />
-                      </div>
-                    </Row>
-                    <Row>
-                      <FormField
-                        className="form-group col-9"
-                        as="select"
-                        name={`questions.${index}.categoryId`}
-                        label="Category"
-                      >
-                        <Fragment>
-                          <option></option>
-                          {categoryState.response?.content &&
-                            categoryState.response.content.map((t, index) => (
-                              <option key={index} value={t.id?.itemId}>
-                                {t.name}
-                              </option>
-                            ))}
-                        </Fragment>
-                      </FormField>
-                      <div className="form-group col-3">
-                        <label htmlFor={`questions.${index}.points`} className="mr-2">
-                          Points
-                        </label>
-                        <Field name={`questions.${index}.points`} className="form-control" type="number" />
-                      </div>
-                    </Row>
-                  </CardBody>
-                </Card>
-              ))}
+                        />
+                      </Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+            </Table>
           </div>
-        </Fragment>
+        </Box>
       )}
     </FieldArray>
   );
@@ -119,40 +117,52 @@ const GuestQuestionEditor = (props: FormikProps<QAISection>) => {
   return (
     <FieldArray name="guestQuestions">
       {({ insert, remove, push }) => (
-        <Fragment>
-          <div
-            className={cx(
-              'mb-2',
-              css`
-                display: flex;
-              `
-            )}
-          >
-            <h4>Guest Questions</h4>
-            <Button
-              className="ml-1"
-              color="info"
-              onClick={() => {
-                push({
-                  text: '',
-                  points: 2,
-                });
-              }}
-            >
-              <Icon name="plus" size="1x" />
-            </Button>
+        <Box p={4}>
+          <div>
+            <Text fontSize="l">Guest Questions</Text>
           </div>
           <div>
-            {props.values.guestQuestions &&
-              props.values.guestQuestions.length > 0 &&
-              props.values.guestQuestions.map((question, index) => (
-                <Card className="card mb-1" key={`guestQuestions-${index}`}>
-                  <CardBody>
-                    <Row>
-                      <div className=" form-group col-1 d-flex align-items-end">
-                        <Button
-                          outline={false}
-                          color="danger"
+            <Table>
+              <TableCaption>
+                Add a new guest question{' '}
+                <IconButton
+                  aria-label="Add Guest Question"
+                  icon={<AddIcon />}
+                  onClick={() => {
+                    push({
+                      text: '',
+                      points: 2,
+                    });
+                  }}
+                />
+              </TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Question Text</Th>
+                  <Th>Points</Th>
+                  <Th>Actions</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {props.values.guestQuestions &&
+                  props.values.guestQuestions.length > 0 &&
+                  props.values.guestQuestions.map((question, index) => (
+                    <Tr key={`guestQuestions.${index}.text`}>
+                      <Td>
+                        <Field
+                          name={`guestQuestions.${index}.text`}
+                          component={FormikTextInput}
+                          placeholder="Enter Question"
+                        />
+                      </Td>
+                      <Td>
+                        <Field name={`guestQuestions.${index}.points`} component={FormikTextInput} />
+                      </Td>
+                      <Td isNumeric>
+                        <IconButton
+                          colorScheme="red"
+                          aria-label="Delete Question"
+                          icon={<DeleteIcon />}
                           onClick={() => {
                             confirm({
                               message: 'Delete this question?',
@@ -162,29 +172,14 @@ const GuestQuestionEditor = (props: FormikProps<QAISection>) => {
                               }
                             });
                           }}
-                        >
-                          <Icon name="trash" size="1x" />
-                        </Button>
-                      </div>
-                      <div className="form-group col-9">
-                        <label htmlFor={`guestQuestions.${index}.text`} className="mr-2">
-                          Text
-                        </label>
-                        <Field name={`guestQuestions.${index}.text`} className="form-control" />
-                      </div>
-
-                      <div className="form-group col-2">
-                        <label htmlFor={`guestQuestions.${index}.points`} className="mr-2">
-                          Points
-                        </label>
-                        <Field name={`guestQuestions.${index}.points`} className="form-control" type="number" />
-                      </div>
-                    </Row>
-                  </CardBody>
-                </Card>
-              ))}
+                        />
+                      </Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+            </Table>
           </div>
-        </Fragment>
+        </Box>
       )}
     </FieldArray>
   );
