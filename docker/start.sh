0000bb5b21b78f4171468e6f557252587724cafd
Jeremy Branham <jeremy@savantly.net>
2021-12-20 11:14:17 -0600

    split FE and BE

--- docker/start.sh
+++ docker/start.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-if [ "true" = "${START_POSTGRES}}" ]; then
-    # start postgres
-    su - postgres -c "/usr/lib/postgresql/12/bin/initdb -D $PGDATA"
-    su - postgres -c "PGDATA=$PGDATA /usr/lib/postgresql/12/bin/pg_ctl -D /db -l logfile start"
-fi
-
-# start nginx in background
-nginx
-
 # copy the modules into the classpath
 cp /sprout/modules/* /sprout/BOOT-INF/lib/
 
 # start the sprout server
-java ${JAVA_OPTS} -cp ${SPROUT_OTHER_CLASSES}:/sprout -Dserver.port=${SERVER_PORT} org.springframework.boot.loader.JarLauncher
+java ${JAVA_OPTS} -cp ${SPROUT_OTHER_CLASSES}:/sprout -Dserver.port=${PORT} org.springframework.boot.loader.JarLauncher
--- scripts/deployHeroku.sh
+++ scripts/deployHeroku.sh
@@ -4,11 +4,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/..
 
-IMAGE_NAME=franchise-manager-demo
-
 heroku container:login
 
-docker tag savantly/${IMAGE_NAME}:latest registry.heroku.com/franchise-manager/web
-docker push registry.heroku.com/franchise-manager/web
+heroku container:push web -a api-franchise-manager
+heroku container:release web -a api-franchise-manager
 
+
+docker tag savantly/sprout-webapp:latest registry.heroku.com/franchise-manager/web
+docker push registry.heroku.com/franchise-manager/web
 heroku container:release web -a franchise-manager
