e9bed924a438a2eb90817f19b385e2fa97732f5c
Jeremy Branham <jeremy@savantly.net>
2021-12-26 19:36:31 -0600

    separate dev files from docker compose
    Mounting the app files in the dev container works, but
    the generated folders and artifacts are owned by root, and it
    makes testing/changes more complicated without sudo permission

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 cd $DIR/..
 
-export REPO_ROOT=$(pwd)
+ENV_FILE=.env
+
+if [ -f "$ENV_FILE" ]; then
+  source $ENV_FILE
+fi
+
 docker-compose -f module/docker-compose.yml up
