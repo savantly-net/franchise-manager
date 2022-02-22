3e5b2aeb6b419879f83a231550caee45a0622346
Jeremy Branham <jeremy@savantly.net>
2022-02-22 13:19:47 -0600

    fixes migration and adds test

--- module/src/main/java/net/savantly/sprout/franchise/db/FMDBMigration.java
+++ module/src/main/java/net/savantly/sprout/franchise/db/FMDBMigration.java
@@ -4,38 +4,66 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.AllArgsConstructor;
-
 @Configuration
-@AllArgsConstructor
 public class FMDBMigration {
 	
 	private static final String SCHEMA_VERSION_TABLE = "fm_schema_version";
 	private final DataSource dataSource;
 	private final Logger log = LoggerFactory.getLogger(FMDBMigration.class);
+	Flyway flyway;
+
+	private boolean autoMigrate = false;
+
+	public FMDBMigration(DataSource ds) {
+		this.dataSource = ds;
+	}
+
+	public FMDBMigration(DataSource ds, boolean auto) {
+		this.autoMigrate = false;
+		this.dataSource = ds;
+	}
+
+	public void setAutoMigrate(boolean auto) {
+		this.autoMigrate = auto;
+	}
 
 	@PostConstruct
-    public void migrate() {
+	public void postConstruct() {
+		configure();
+		if (this.autoMigrate) {
+			migrate();
+		}
+	}
+
+	public void configure() {
 		DataSource source = (DataSource) dataSource;
-        Flyway flyway = Flyway.configure()
-        		.dataSource(source)
-        		.locations(String.format("classpath:/fm/db/%s", getDbType(dataSource)))
-        		// don't clean the db out!!!
-        		.cleanDisabled(true)
-        		.table(SCHEMA_VERSION_TABLE)
-        		.baselineOnMigrate(true)
-        		.baselineVersion("0")
-        		.load();
+		flyway = Flyway.configure()
+		.dataSource(source)
+		.locations(String.format("classpath:/fm/db/%s", getDbType(dataSource)))
+		// don't clean the db out!!!
+		.cleanDisabled(true)
+		.table(SCHEMA_VERSION_TABLE)
+		.baselineOnMigrate(true)
+		.baselineVersion("0")
+		.load();
+	}
+
+    public void migrate() {
         try {
             flyway.migrate();
         } catch (Exception e) {
         	log.error("failed to migrate frachise manager", e);
         }
     }
+
+	public MigrationInfoService getMigrationInfo() {
+		return flyway.info();
+	}
 
 	//TODO: detect correct db type
 	private String getDbType(DataSource dataSource2) {
---
+++ module/src/test/java/net/savantly/sprout/franchise/db/FMDBMigration_test.java
@@ -0,0 +1,58 @@
+package net.savantly.sprout.franchise.db;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.postgresql.jdbc3.Jdbc3SimpleDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import test.AbstractContainerBaseTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class FMDBMigration_test extends AbstractContainerBaseTest {
+
+    @Autowired
+    FMDBMigration mig;
+
+    /**
+     * This needs to be update for any new migrations
+     */
+    final String expectedVersion = "0.0.7";
+
+    @Test
+    public void testMigration() {
+        mig.migrate();
+        Assertions.assertEquals(expectedVersion, mig.getMigrationInfo().current().getVersion().getVersion());
+    }
+    
+    
+    @TestConfiguration
+    static class testConfig {
+
+        @Bean
+        public FMDBMigration migration(DataSource dataSource) {
+            return new FMDBMigration(dataSource, false);
+        }
+
+        @Bean
+        public DataSource datasource(Environment env) {
+            PGSimpleDataSource ds = new PGSimpleDataSource();
+            ds.setUser(env.getProperty("spring.datasource.username"));
+            ds.setPassword(env.getProperty("spring.datasource.password"));
+            ds.setURL(env.getProperty("spring.datasource.url"));
+            return ds;
+        }
+        
+    }
+}
+
