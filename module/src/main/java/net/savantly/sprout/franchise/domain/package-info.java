3cbece2c8912d4e91388312a6bfd79df86052b9f
Jeremy <jeremy@savantly.net>
2022-12-26 23:00:39 -0600

    remove package info

--- module/src/main/java/net/savantly/sprout/franchise/domain/package-info.java
+++
@@ -1,4 +0,0 @@
-@org.hibernate.annotations.TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
-package net.savantly.sprout.franchise.domain;
-
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
